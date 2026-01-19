f3b83f3ba9b3c202232593d29cc4026c8837b7c5
guiireal <guii_real@hotmail.com>
2026-01-18 23:56:17 -0300

    wip

--- src/commands/member/funny/dado.js
+++ src/commands/member/funny/dado.js
@@ -6,7 +6,7 @@
  */
 import { delay } from "baileys";
 import path from "node:path";
-import { ASSETS_DIR, PREFIX } from "../../config.js";
+import { ASSETS_DIR, PREFIX } from "../../../config.js";
 import { DangerError } from "../../errors/index.js";
 import { getRandomNumber } from "../../utils/index.js";
 
@@ -32,7 +32,7 @@ export default {
 
     if (!number || number < 1 || number > 6) {
       throw new DangerError(
-        `¡Por favor, elige un número entre 1 y 6!\nEjemplo: ${PREFIX}dado 3`
+        `¡Por favor, elige un número entre 1 y 6!\nEjemplo: ${PREFIX}dado 3`,
       );
     }
 
@@ -43,7 +43,7 @@ export default {
     const pushName = webMessage?.pushName || "Usuário";
 
     await sendStickerFromURL(
-      path.resolve(ASSETS_DIR, "stickers", "dice", `${result}.webp`)
+      path.resolve(ASSETS_DIR, "stickers", "dice", `${result}.webp`),
     );
 
     await delay(2000);
@@ -51,12 +51,12 @@ export default {
     if (number === result) {
       await sendReact("🏆");
       await sendReply(
-        `🎉 *${pushName} ¡GANÓ!* Apostaste al número *${number}* y el dado cayó en *${result}*! 🍀`
+        `🎉 *${pushName} ¡GANÓ!* Apostaste al número *${number}* y el dado cayó en *${result}*! 🍀`,
       );
     } else {
       await sendReact("😭");
       await sendReply(
-        `💥 *${pushName} PERDIÓ...* Apostaste al *${number}* pero el dado cayó en *${result}*! Intenta de nuevo.`
+        `💥 *${pushName} PERDIÓ...* Apostaste al *${number}* pero el dado cayó en *${result}*! Intenta de nuevo.`,
       );
     }
   },
