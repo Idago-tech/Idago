9b09bc1e402fccf20987fb5765c62ed9a0ca3ea1
Idago-tech <e61973625@gmail.com>
2026-03-26 23:46:36 -0400

    Actualizar logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,37 +1,32 @@
-/**
- * Logs
- *
- * @author Dev Gui
- */
 import pkg from "../../package.json" with { type: "json" };
 
 export function sayLog(message) {
-  console.log("\x1b[36m[TAKESHI BOT | TALK]\x1b[0m", message);
+  console.log("\x1b[36m[TRC BOT | TALK]\x1b[0m", message);
 }
 
 export function inputLog(message) {
-  console.log("\x1b[30m[TAKESHI BOT | INPUT]\x1b[0m", message);
+  console.log("\x1b[30m[TRC BOT | INPUT]\x1b[0m", message);
 }
 
 export function infoLog(message) {
-  console.log("\x1b[34m[TAKESHI BOT | INFO]\x1b[0m", message);
+  console.log("\x1b[34m[TRC BOT | INFO]\x1b[0m", message);
 }
 
 export function successLog(message) {
-  console.log("\x1b[32m[TAKESHI BOT | SUCCESS]\x1b[0m", message);
+  console.log("\x1b[32m[TRC BOT | SUCCESS]\x1b[0m", message);
 }
 
 export function errorLog(message) {
-  console.log("\x1b[31m[TAKESHI BOT | ERROR]\x1b[0m", message);
+  console.log("\x1b[31m[TRC BOT | ERROR]\x1b[0m", message);
 }
 
 export function warningLog(message) {
-  console.log("\x1b[33m[TAKESHI BOT | WARNING]\x1b[0m", message);
+  console.log("\x1b[33m[TRC BOT | WARNING]\x1b[0m", message);
 }
 
 export function bannerLog() {
-  console.log(`\x1b[36m░▀█▀░█▀█░█░█░█▀▀░█▀▀░█░█░▀█▀░░█▀▄░█▀█░▀█▀\x1b[0m`);
-  console.log(`░░█░░█▀█░█▀▄░█▀▀░▀▀█░█▀█░░█░░░█▀▄░█░█░░█░`);
-  console.log(`\x1b[36m░░▀░░▀░▀░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░░▀▀░░▀▀▀░░▀░\x1b[0m`);
-  console.log(`\x1b[36m🤖 Versión: \x1b[0m${pkg.version}\n`);
+  console.log(`\x1b[36m░▀█▀░█▀▄░█▀▀░░█▀▄░█▀█░▀█▀\x1b[0m`);
+  console.log(`░░█░░█▀▄░█░░░░█▀▄░█░█░░█░`);
+  console.log(`\x1b[36m░░▀░░▀░▀░▀▀▀░░▀▀░░▀▀▀░░▀░\x1b[0m`);
+  console.log(`\x1b[36mVersión: \x1b[0m${pkg.version}\n`);
 }
